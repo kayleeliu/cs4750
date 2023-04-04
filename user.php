280bd06f5015ad9f9861dabd956c971ddba370f6
Candace Lam Chen <candacechen12@gmail.com>
2023-04-04 18:18:51 -0400

    insert into db

--- user.php
+++ user.php
@@ -1,11 +1,11 @@
 <?php
 function addUser($username, $password, $daily_calorie_count=NULL, $meals_per_day=NULL) {
     global $db;
-    $hash = password_hash(password, PASSWORD_DEFAULT);
+    // $hash = password_hash($password, PASSWORD_DEFAULT);
     $query = "insert into User (username, password, daily_calorie_count, meals_per_day) values (:username, :password, :daily_calorie_count, :meals_per_day)";
     $statement = $db->prepare($query);
     $statement->bindValue(':username', $username);
-    $statement->bindValue(':password', $hash);
+    $statement->bindValue(':password', $password);
     $statement->bindValue(':daily_calorie_count', $daily_calorie_count);
     $statement->bindValue(':meals_per_day', $meals_per_day);
     $statement->execute();
