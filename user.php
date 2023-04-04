2a37fedb85ac86080ad852b1832efe55446058aa
Justin Mao Zhang <jmz8rm@portal04.cs.virginia.edu>
2023-04-04 17:35:54 -0400

    completed addUser

--- user.php
+++ user.php
@@ -1,11 +1,13 @@
 <?php
 function addUser($username, $password, $daily_calorie_count=NULL, $meals_per_day=NULL) {
     global $db;
+    $hash = password_hash(password, PASSWORD_DEFAULT);
     $query = "INSERT INTO User(username, password, daily_calorie_count, meals_per_day) VALUES (:username, :password, :daily_calorie_count, :meals_per_day)";
     $statement = $db->prepare($query);
-    $statement->bindValue(':name', $name);
-    $statement->bindValue(':major', $major);
-    $statement->bindValue(':year', $year);
+    $statement->bindValue(':username', $username);
+    $statement->bindValue(':password', $hash);
+    $statement->bindValue(':daily_calorie_count', $daily_calorie_count);
+    $statement->bindValue(':meals_per_day', $meals_per_day);
     $statement->execute();
     $statement->closeCursor();
 }
