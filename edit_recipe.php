cd803276c56cc2ccae7c88d4f43658a7135b9c91
jmz8rrm <jmz8rm@virginia.edu>
2023-05-01 21:19:21 -0400

    changed recipe ingredients to quantity

--- edit_recipe.php
+++ edit_recipe.php
@@ -106,7 +106,7 @@ $foods = getFoodsRecipeUses($recipeID);
           <div class="row mb-3 mx-3"> Name:
             <input type="text" class="form-control" name="name" required />     
           </div>  
-          <div class="row mb-3 mx-3">Servings: 
+          <div class="row mb-3 mx-3">Quantity: 
             <input type="number" class="form-control" name="quantity" required />    
           </div>  
           <div class="row mb-3 mx-3">Units: 
@@ -124,7 +124,7 @@ $foods = getFoodsRecipeUses($recipeID);
       <thead>
       <tr style="background-color:#B0B0B0">
         <th width="20%"> Name    
-        <th width="20%"> Servings
+        <th width="20%"> Quantity
         <th width="20%"> Units
         <th width="20%"> Delete
       </tr>
